9f41065fdf71e6ecd629fe31cc5da9ac3a69f7a6
gdougal <gdougal@student.21-school.ru>
2021-01-22 01:49:40 +0300

    add return value in parser

--- philo_one/srcs/philo_pars.c
+++ philo_one/srcs/philo_pars.c
@@ -15,20 +15,24 @@
 int				forks_init(t_info *info)
 {
 	int	i;
+	int k;
 
 	i = 0;
+	k = 0;
 	if (!(info->forks = malloc((info->rules[SUM_PH]) * sizeof(pthread_mutex_t))))
 		return (3);
 	while (i < info->rules[SUM_PH])
 	{
-		if (pthread_mutex_init(&info->forks[i], NULL))
+		if ((k = pthread_mutex_init(&info->forks[i], NULL)))
 			return (4);
 		i++;
 	}
-	pthread_mutex_init(&info->write, NULL);
-	pthread_mutex_init(&info->l_check, NULL);
-	pthread_mutex_init(&info->last_eat, NULL);
-	pthread_mutex_init(&info->chng, NULL);
+	k = pthread_mutex_init(&info->write, NULL);
+	k = pthread_mutex_init(&info->l_check, NULL);
+	k = pthread_mutex_init(&info->last_eat, NULL);
+	k = pthread_mutex_init(&info->chng, NULL);
+	if (k)
+		return (4);
 	info->start = 0;
 	return (0);
 }
